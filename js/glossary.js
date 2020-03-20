fe464845f3332f9d1ce92291e5bb2dd5ab79dd5f
Brennan <bproorda@gmail.com>
2020-03-19 19:16:07 -0500

    glossary pages now displays what percent user has gotten right

--- js/glossary.js
+++ js/glossary.js
@@ -9,10 +9,11 @@ var percents = [];
 console.log(attempted);
 console.log(correct);
 
+
 function progressTracker() {
   //filling percents array
   for (var i = 0; i < 30; i++) {
-    var percentNumber =(Math.floor(correct[i] / attempted[i] )*100);
+    var percentNumber = (correct[i] / attempted[i] ) * 100;
     if (isNaN(percentNumber)) {
       percentNumber = 0;
       percents.push(percentNumber);
@@ -21,7 +22,9 @@ function progressTracker() {
     }
   }
   for (var j = 0; j < 30; j++) {
-    var defineNode = document.getElementById('define' + j);
+    var defineNode = document.getElementById('define' + (j+1));
+    var childNode = defineNode.childNodes[1];
+    // console.log(childNode.textContent);
     if (percents[j] === 100) {
       defineNode.style.backgroundColor = 'gold';
     } else if (percents[j] < 100 && percents[j] >= 70) {
@@ -30,6 +33,12 @@ function progressTracker() {
       defineNode.style.backgroundColor = 'yellow';
     } else if (percents[j] < 50 && percents[j] > 0) {
       defineNode.style.backgroundColor = 'red';
+    }
+    if (percents[j] > 0) {
+      var progressText = document.createElement('p');
+      progressText.textContent = 'You got this one right ' + percents[j] + '% of the time.';
+      //childNode.textContent = childNode.textContent + '  You got this one right ' + percents[j] + '% of the time.';
+      childNode.appendChild(progressText);
     }
   }
 
